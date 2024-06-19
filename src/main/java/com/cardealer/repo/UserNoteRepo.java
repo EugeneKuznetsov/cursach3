16203899263ec836ba92f7c9ed17d70e19b89f44
EugeneKuznetsov <113844541+EugeneKuznetsov@users.noreply.github.com>
2024-06-19 21:21:48 +0300

    Delete src/main/java/com/cardealer directory

--- src/main/java/com/cardealer/repo/UserNoteRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cardealer.repo;
-
-import com.cardealer.model.UserNote;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserNoteRepo extends JpaRepository<UserNote, Long> {
-}
